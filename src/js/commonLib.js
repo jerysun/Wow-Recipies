d0ca3ceb38cd1ff80d25eedc2098b495fe97e09e
Jerry Sun <jerysun007@hotmail.com>
2020-03-01 22:35:52 +0100

    Adjusted the format

--- src/js/commonLib.js
+++ src/js/commonLib.js
@@ -15,19 +15,19 @@ export const oneDecimalInfiniteToFraction = dec => {
 
     decTen = parseInt(decTen.toString().split('.')[0], 10);
     decCpy = parseInt(decCpy.toString().split('.')[0], 10);
-	const diff = decTen - decCpy;
-	const res = new Fraction(diff, 9).toString();
-	return res;
+    const diff = decTen - decCpy;
+    const res = new Fraction(diff, 9).toString();
+    return res;
 };
 
 // A decimal with two-place infinite. e.g. 0.0343434 = 34/990 = 17/495
 export const twoDecimalInfiniteToFraction = dec => {
-	let decCpy = dec; // 0.0343434
-	let decCen = dec * 100;// 3.43434
+    let decCpy = dec; // 0.0343434
+    let decCen = dec * 100;// 3.43434
 
-	decCpy = parseFloat(decCpy.toString().substring(0, 3));
-	decCen = parseFloat(decCen.toString().substring(0, 3));
-	const diff = decCen - decCpy;
-	const res = new Fraction(diff, 99).toString();
-	return res;
+    decCpy = parseFloat(decCpy.toString().substring(0, 3));
+    decCen = parseFloat(decCen.toString().substring(0, 3));
+    const diff = decCen - decCpy;
+    const res = new Fraction(diff, 99).toString();
+    return res;
 };
